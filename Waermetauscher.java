b6247c2d7bf26050787d3947f1cfc26fba476e7a
Friederike <F.herbold@googlemail.com>
2015-01-08 13:53:44 +0100

    Implementiert

--- Waermetauscher.java
+++ Waermetauscher.java
@@ -4,5 +4,22 @@ package ws2014.tpe.gruppe_1415349_1410206.uebung5;
  *
  */
 public class Waermetauscher {
+	
+	public void waermeTauschen(WasserElement kuehlung, WasserElement zuKuehlen) {
+		zuKuehlen.setTemperatur(berechne(kuehlung,zuKuehlen));
+	}
 
+	/**
+	 * 
+	 * @param kuehlung
+	 * @param zuKuehlen
+	 * @return
+	 */
+	private int berechne(WasserElement kuehlung, WasserElement zuKuehlen) {
+		int tempEingang = zuKuehlen.getTemperatur();
+		int tempKuehlwasser = kuehlung.getTemperatur();
+		int tempAusgang = (tempEingang + tempKuehlwasser) / 2;
+
+		return tempAusgang;
+	}
 }
