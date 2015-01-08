a825b12938abfe36fbfa2d53ebb4f4d224e04f9d
Friederike <F.herbold@googlemail.com>
2015-01-08 13:54:56 +0100

    Erzeugt für Kernschmelze

---
+++ KernschmelzeException.java
@@ -0,0 +1,13 @@
+package ws2014.tpe.gruppe_1415349_1410206.uebung5;
+
+/**
+ * 
+ *
+ */
+public class KernschmelzeException extends Exception {
+
+	public KernschmelzeException() {
+		super();
+	}
+
+}
