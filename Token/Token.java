59b3af47134673a068ae058f65cb9f0a3a127988
Oksana <oksana.semeshkina@stud.hs-mannheim.de>
2015-01-12 18:09:21 +0100

    Tokens sind erzeugt

---
+++ Token/Token.java
@@ -0,0 +1,7 @@
+package ws2014.tpe.gruppe_1415349_1410206.uebung5.Token;
+
+
+public class Token {
+public static Tausch tausch= new Tausch();
+public static Durchgang durchgang= new Durchgang();
+}
