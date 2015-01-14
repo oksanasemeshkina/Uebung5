4fc3b763fece6cca04b461064021eaceb1cd704e
Friederike <F.herbold@googlemail.com>
2015-01-14 16:37:12 +0100

    erzeugt

---
+++ Simulation/KraftwerkTest.java
@@ -0,0 +1,14 @@
+package ws2014.tpe.gruppe_1415349_1410206.uebung5.Simulation;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class KraftwerkTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
